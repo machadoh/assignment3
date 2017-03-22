4229f4b9607f1ce1c3a423570575ab0b0b98e55e
machadoh <hhm4@pitt.edu>
2017-03-22 15:26:42 -0400

    Update assignment3.php

--- assignment3.php
+++ assignment3.php
@@ -1,6 +1,10 @@
 <?php 
 
-	
+	spl_autoload_register(function ($class_name) {
+			include $class_name . '.php';
+	});
+
+	$handa = new ChildClass("Henrique", "Student", "Brazil");
 
 ?>
 
@@ -13,6 +17,23 @@
 	<link rel="stylesheet" href="assignment3.css">
 </head>
 <body>
+	<h1> Henrique Machado </h1>
 	
+	<section>
+		<p> This is my first PHP Assignment</p>
+		<p> It some how works!</p>
+		
+		<p> --------------------------------------</p>
+		<p class = "output"><?= $handa; ?></p>
+		
+		<p>But now he moved to USA </p>
+		<?= $handa->updatecountry("USA"); ?>
+
+		<p>So now,</p>
+		<p class = "output"><?= $handa; ?></p>
+		<p> Enjoy your new experience abroad</p>
+		
+		
+	</section>
 </body>
 </html>
